27b478108a74c4a83c044e8a0acf352c8f02f67c
John Arboleda <JohnJack0126@gmail.com>
2021-05-21 17:01:43 -0500

    Create view for New Post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,8 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create]
+
+  # GET /posts/new
+  def new
+    @post = current_user.posts.build
+  end
 end
