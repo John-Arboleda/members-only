21a6cd4b703f9c62b806f6ef53f61bbb89090a77
John Arboleda <JohnJack0126@gmail.com>
2021-05-26 11:39:12 -0500

    Create index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,31 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, only: %i[new create]
 
+  # GET /posts
+  def index
+    @posts = Post.all.order('created_at DESC')
+    @post = Post.new
+  end
+
   # GET /posts/new
   def new
     @post = current_user.posts.build
+  end
+
+  # POST /posts
+  def create
+    @post = current_user.posts.build(post_params)
+
+    if @post.save
+      redirect_to root_path, notice: 'Post successfully created'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:body)
   end
 end
