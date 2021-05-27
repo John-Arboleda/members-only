d2a1e6d60e5ab2c20d751753c683d7418501e1d9
John Arboleda <JohnJack0126@gmail.com>
2021-05-26 22:45:24 -0500

    modify routes file

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,17 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
+  private
 
-  def create
-    super
+  def sign_up_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
   end
-
-  protected
-
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-  end
-
-  # def sign_up_params
-  #   params.require(:user).permit(:username, :email, :password, :password_confirmation)
-  # end
 end
