6ecbda5f1fed9ec48d65cea97f34b3c22acb5c5a
John Arboleda <JohnJack0126@gmail.com>
2021-05-28 18:02:12 -0500

    Correct sign out controller error

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   
   root "posts#index"
-  devise_for :users, controllers: {registrations: 'registrations'}
+  devise_for :users, controllers: {registrations: 'registrations', sessions: 'sessions'}
   resources :posts, only: [:new, :create, :index]
 end
